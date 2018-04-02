8d1da00eda17bbfe31fcfa815ae38169309e484c
Hider <dtwdevelop@gmail.com>
2018-04-02 10:48:31 +0100

    add fix all fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBN8mYwWWe5qs8VpJTw_LNFObtVuh-s-vE",
+    authDomain: "demos-1081.firebaseapp.com",
+    databaseURL: "https://demos-1081.firebaseio.com",
+    projectId: "demos-1081",
+    storageBucket: "",
+    messagingSenderId: "955741029846"
+  }
 };
